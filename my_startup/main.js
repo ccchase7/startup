070f8a37295463a36ce6a6c97e834663ab5aa65a
Clayton Chase <ccchase7@byu.edu>
2023-03-25 00:41:33 -0600

    Progress on startup javascript

--- my_startup/main.js
+++ my_startup/main.js
@@ -4,11 +4,18 @@ class AnagramBuilder
 {
     outputTextBox;
     inputTextBox;
+    sidebar;
+    id_count;
+    savedAnagrams;
+    
 
     constructor()
     {
         this.outputTextBox = document.getElementById("answer-text");
         this.inputTextBox = document.getElementById("input-textbox");
+        this.sidebar = document.getElementById("saved-anagrams");
+        this.id_count = 0;
+        this.savedAnagrams = [];
     }
 
     getAnagram()
@@ -23,13 +30,55 @@ class AnagramBuilder
         return "No anagram found for " + input;
     }
 
+    async loadSavedAnagrams()
+    {
+        let noAnagrams = await makeNewSavedAnagram("You have no saved Anagrams.")
+        this.sidebar.appendChild(noAnagrams);
+        return 0;
+    }
+
+    async getNextId()
+    {
+        return this.id_count++;
+    }
+
 }
 
 const anagramBuilder = new AnagramBuilder();
+anagramBuilder.loadSavedAnagrams();
+inbox = anagramBuilder.inputTextBox;
+outbox = anagramBuilder.outputTextBox;
 
 function clearInputOutput()
 {
     anagramBuilder.inputTextBox.value = "";
     anagramBuilder.outputTextBox.textContent = "";
     anagramBuilder.inputTextBox.focus();
+}
+
+async function saveAnagram()
+{
+    let sidebar = anagramBuilder.sidebar;
+
+    if (anagramBuilder.savedAnagrams.length === 0)
+    {
+        while (anagramBuilder.sidebar.firstChild)
+        {
+            anagramBuilder.sidebar.removeChild(anagramBuilder.sidebar.firstChild);
+        }
+    }
+
+    let currAnagram = await makeNewSavedAnagram(anagramBuilder.outputTextBox.textContent);
+
+    anagramBuilder.savedAnagrams.push(currAnagram);
+    sidebar.appendChild(currAnagram);
+}
+
+async function makeNewSavedAnagram(txt)
+{
+    let newAnagram = document.createElement("div");
+    newAnagram.classList.add("card");
+    newAnagram.id = await anagramBuilder.getNextId();
+    newAnagram.textContent = txt;
+    return newAnagram;
 }
